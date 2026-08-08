c1ffb640e8fbac9ac07b075b22ce37079547eb1b
Totto16 <tobiausgais@gmail.com>
2026-08-09 00:42:08 +0200

    fix: fix locale_t compat layer, don't have the implementation in the header

---
+++ libcxx/src/__support/uefi/locale_t.c
@@ -0,0 +1,33 @@
+
+#include "__support/uefi/locale_t"
+
+static struct __uefi_locale __uefi_C_locale_storage;
+locale_t __uefi_C_locale = &__uefi_C_locale_storage;
+
+locale_t newlocale(int mask, const char* locale, locale_t base) {
+  (void)mask;
+  (void)locale;
+  (void)base;
+
+  return __uefi_C_locale;
+}
+
+locale_t duplocale(locale_t loc) {
+  (void)loc;
+
+  return __uefi_C_locale;
+}
+
+void freelocale(locale_t loc) { (void)loc; }
+
+static locale_t __uefi_current_locale = &__uefi_C_locale_storage;
+
+locale_t uselocale(locale_t loc) {
+  locale_t old = __uefi_current_locale;
+
+  if (loc != (locale_t)0) {
+    __uefi_current_locale = loc;
+  }
+
+  return old;
+}
